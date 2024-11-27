0eb21fa63b864aab9008e12dde603150ca645d30
klochkov2607 <klochkov2607@gmail.com>
2024-11-27 23:38:29 +0300

    Создана функция exchange для получения из интернета курсов валют

--- Obmem.py
+++ Obmem.py
@@ -1,17 +1,32 @@
-'''import requests
-import json
-import pprint
-
-result=requests.get("https://open.er-api.com/v6/latest/USD")
-data=json.loads(result.text)
-p=pprint.PrettyPrinter(indent=4)# indent, количество отступов
-
-p.pprint(data)'''
+from logging import exception
+from unittest import expectedFailure
 
 import requests
 import json
 from tkinter import*
 from tkinter import messagebox as mb
+
+from bottle import response
+
+
+def exchange():
+    code=entry.get()
+
+    if code:
+        try:
+            response=requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()# raise_for_status, если все хорошо
+            data=response.json()
+            if code in data["rates"]:
+                exchange_rate=data["rates"][code]
+                mb.showinfo("Курс обмена", f"Курс:{exchange_rate} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты!")
+
 
 # import pprint# функция pprint является основной функцией модуля и позволяет красиво выводить данные на экран
 
